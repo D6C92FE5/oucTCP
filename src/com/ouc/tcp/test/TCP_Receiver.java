7cd30ce5bb23f9b5e95db05868113afbc4fbf258
D6C92FE5 <D6C92FE5@hotmail.com>
2013-12-23 21:42:32 +0800

    RDT 1.0

--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -10,7 +10,9 @@ import com.ouc.tcp.message.*;
 import com.ouc.tcp.tool.TCP_TOOL;
 
 public class TCP_Receiver extends TCP_Receiver_ADT {
-		
+	
+	private TCP_PACKET ackPack;	//回复的ACK报文段
+	
 	/*构造函数*/
 	public TCP_Receiver() {
 		super();	//调用超类构造函数
@@ -18,19 +20,29 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	}
 
 	@Override
-	//接收到数据报：检查校验和，设置回复的ACK报文段
+	//接收到数据报：设置回复的ACK报文段
 	public void rdt_recv(TCP_PACKET recvPack) {
-		//生成ACK报文段（设置确认号/校验和归零）
+		//生成ACK报文段（设置确认号）
+		tcpH.setTh_ack(recvPack.getTcpH().getTh_seq());
+		ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
 		
-		//调用reply()回复ACK报文段
+		//回复ACK报文段
+		reply(ackPack);
 		
-		//将数据插入data队列，交付数据
+		//将接收到的正确有序的数据插入data队列，准备交付
+		dataQueue.add(recvPack.getTcpS().getData());
+		
+		//交付数据（每20组数据交付一次）
+		if(dataQueue.size() == 20) {
+			deliver_data();
+		}
+		
+		System.out.println();
 	}
 
 	@Override
-	//交付数据（将数据写入文件）
-	public void deliver_data() {
-		//检查dataQueue，将数据写入文件
+	//交付数据（将接收到的正确有序的数据写入文件）
+	public void deliver_data() {	
 		File fw = new File("recvData.txt");
 		BufferedWriter writer;
 		
@@ -48,6 +60,7 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 				
 				writer.flush();		//清空输出缓存
 			}
+			
 			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -59,11 +72,10 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	//回复ACK报文段
 	public void reply(TCP_PACKET replyPack) {
 		//设置错误控制标志
-		
-		//计算校验和，设置TCP首部重新打包
+		tcpH.setTh_eflag((byte)0);	//eFlag=0，信道无错误
 		
 		//发送数据报
-		//client.send(tcpPack);
+		client.send(replyPack);		
 	}
 	
 }
